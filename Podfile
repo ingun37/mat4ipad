3f6a85d8d7c870a2c934173d04e86ab109d500d1
ingun <ingun37@gmail.com>
2019-05-29 13:49:40 +0900

    init

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '12.2'
+
+target 'mat4ipad' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for mat4ipad
+  pod 'iosMath'
+  target 'mat4ipadTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'mat4ipadUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- mat4ipad/ViewController.swift
+++ mat4ipad/ViewController.swift
@@ -7,14 +7,68 @@
 //
 
 import UIKit
+import iosMath
 
+protocol item {
+    
+}
+enum Err:Error {
+    case stackisempty
+    case unknownArith
+}
+
+protocol Exp: item {
+    func latex() -> String;
+}
+protocol BinaryOp:Exp {
+    var a: Exp { get }
+    var b: Exp { get }
+}
+struct Mul:BinaryOp {
+    var a: Exp
+    
+    var b: Exp
+    
+    func latex() -> String {
+        return "{\(a.latex())} * {\(b.latex())}"
+    }
+}
+struct Mat:Exp {
+    func latex() -> String {
+        return """
+                \\begin{pmatrix}
+                a & b \\\\
+                c & d
+                \\end{pmatrix}
+        """
+    }
+    
+    var elements:[[Exp]];
+}
+struct Unassigned:Exp {
+    func latex() -> String {
+        return letter
+    }
+    
+    var letter:String
+    
+}
 class ViewController: UIViewController {
-
+    var exp:Exp = Mul(a: Mat(elements: []), b: Unassigned(letter: "A"));
+    
+    @IBOutlet weak var mathcontainer: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let mathlbl = MTMathUILabel()
+        mathlbl.frame = mathcontainer.frame
+        mathcontainer.addSubview(mathlbl)
+        mathlbl.latex = exp.latex()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func mul(_ sender: Any) {
+        
+    }
 }
 
