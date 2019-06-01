e47dc946184ced9de4b69001ba8962844c024d6e
ingun <ingun37@gmail.com>
2019-06-01 13:12:46 +0900

    matview

--- mat4ipad/ExpTreeView.swift
+++ mat4ipad/ExpTreeView.swift
@@ -12,11 +12,15 @@ import iosMath
 protocol ExpTreeDelegate {
     func onTap(exp:Exp)
 }
+import RxSwift
+import RxCocoa
+
 class ExpTreeView: UIView {
     var del:ExpTreeDelegate?
     @IBOutlet weak var outstack: UIStackView!
     @IBOutlet weak var stack: UIStackView!
     
+    @IBOutlet weak var matcollection: MatCollection!
     var contentView:UIView?
 
     @IBAction func ontap(_ sender: Any) {
@@ -44,6 +48,7 @@ class ExpTreeView: UIView {
         layer.shadowOpacity = 0.5
         layer.shadowOffset = CGSize(width: 1, height: 1)
         layer.shadowRadius = 1
+        
 //        layer.masksToBounds = false
     }
     func loadViewFromNib() -> UIView? {
@@ -63,11 +68,19 @@ class ExpTreeView: UIView {
             mathlbl.latex = exp.latex()
             mathlbl.sizeToFit()
             outstack.insertArrangedSubview(mathlbl, at: 0)
-            exp.kids.forEach({e in
-                let v = ExpTreeView()
-                v.setExp(exp: e, del:del)
-                stack.addArrangedSubview(v)
-            })
+            
+            if let exp = exp as? Mat {
+                matcollection.isHidden = false
+                stack.isHidden = true
+            } else {
+                matcollection.isHidden = true
+                stack.isHidden = false
+                exp.kids.forEach({e in
+                    let v = ExpTreeView()
+                    v.setExp(exp: e, del:del)
+                    stack.addArrangedSubview(v)
+                })
+            }
         }
     }
     
@@ -81,5 +94,39 @@ extension Range where Bound == Double {
     }
     func block(_ numerator:Int, _ denominator:Int) -> Range<Double> {
         return interp(numerator, denominator)..<interp(numerator+1, denominator)
+    }
+}
+class MatCollection:UICollectionView, UICollectionViewDelegateFlowLayout {
+    let disposeBag = DisposeBag()
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        register(UINib(nibName: "MatCell", bundle: Bundle(for: MatCell.self)), forCellWithReuseIdentifier: "cell")
+        
+        let items = Observable.just(
+            (0..<4).map { "\($0)" }
+        )
+        
+        items.bind(to: self.rx.items(cellIdentifier: "cell", cellType: MatCell.self)) { (row, element, cell) in
+            
+            }
+            .disposed(by: disposeBag)
+        self.delegate = self
+
+    }
+    func collectionView(_ cv: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: frame.size.width/2, height: frame.size.height/2)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
+class MatCell:UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
     }
 }
