d5d46ae9f7de250906cb1fa76170797282cb7dfd
ingun <ingun37@gmail.com>
2019-07-28 21:14:58 +0900

    drawing into matrix cell

--- mat4ipad/Matrix/MatrixViews.swift
+++ mat4ipad/Matrix/MatrixViews.swift
@@ -7,14 +7,77 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
-class MatrixCell: UIView, ExpViewable {
+class MatrixCell: UIView, ExpViewable, UIGestureRecognizerDelegate {
+    @IBOutlet weak var imgView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         translatesAutoresizingMaskIntoConstraints = false
 
         
+//        isMultipleTouchEnabled = true
     }
+    
+    var drawings:[[CGPoint]] = []
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        guard let context: CGContext = UIGraphicsGetCurrentContext() else { return }
+        
+        for drawing in drawings {
+            if drawing.count == 1 {
+                print("dfghjk")
+                context.addEllipse(in: CGRect(origin: drawing[0], size: CGSize(width: 2, height: 2)))
+            } else {
+                context.addLines(between: drawing)
+            }
+        }
+        context.setLineCap(.round)
+        context.setBlendMode(.normal)
+        context.setLineWidth(2)
+        context.setStrokeColor(UIColor.black.cgColor)
+        context.strokePath()
+        context.setFillColor(UIColor.red.cgColor)
+        context.drawPath(using: .fillStroke)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        print("fuck!")
+        drawings.append([touches.first!.location(in: self)])
+        setNeedsDisplay()
+        timer?.invalidate()
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        super.touchesMoved(touches, with: event)
+        guard !drawings.isEmpty else {return}
+        guard let loc = touches.first?.location(in: self) else {return}
+        if let p = drawings.last?.last {
+            if 1 > abs(p.x - loc.x) + abs(p.y - loc.y) {
+                return
+            }
+        }
+        print("move")
+        drawings[drawings.count-1].append(loc)
+        setNeedsDisplay()
+    }
+    var timer:Timer? = nil
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: {[unowned self] (tmr) in
+            self.flushDrawing()
+        })
+    }
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        flushDrawing()
+    }
+    func flushDrawing() {
+        drawings = []
+        setNeedsDisplay()
+    }
+    
     var exp:Exp = Unassigned("z")
     var del:ExpViewableDelegate?
     func set(_ exp:Exp, del:ExpViewableDelegate?) {
