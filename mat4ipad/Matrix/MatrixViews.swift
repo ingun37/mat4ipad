c09316f424d4e6409dae00a3b8e11bfc773f2e05
ingun <ingun37@gmail.com>
2019-08-01 00:16:57 +0900

    digit recognition

--- mat4ipad/Matrix/MatrixViews.swift
+++ mat4ipad/Matrix/MatrixViews.swift
@@ -12,7 +12,7 @@ import RxSwift
 import ByClassRecognizer
 
 class MatrixCell: UIView, ExpViewable, UIGestureRecognizerDelegate {
-    @IBOutlet weak var imgView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         translatesAutoresizingMaskIntoConstraints = false
@@ -20,65 +20,85 @@ class MatrixCell: UIView, ExpViewable, UIGestureRecognizerDelegate {
         
 //        isMultipleTouchEnabled = true
     }
-    let overwriter = Overwriter()
+//    let overwriter = Overwriter()
     
-    func drawTo(context: CGContext, strokeColor:UIColor) {
-        context.addPath(overwriter.drawing)
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        guard !drawing.isEmpty else {
+            return
+        }
+        guard let context: CGContext = UIGraphicsGetCurrentContext() else { return }
+        UIGraphicsPushContext(context)
+        context.addPath(drawing)
         context.setLineCap(.round)
         context.setBlendMode(.normal)
         context.setLineWidth(2)
-        context.setStrokeColor(strokeColor.cgColor)
+        context.setStrokeColor(UIColor.black.cgColor)
         context.strokePath()
+        UIGraphicsPopContext()
     }
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
-        guard let context: CGContext = UIGraphicsGetCurrentContext() else { return }
-        drawTo(context: context, strokeColor: UIColor.black)
-    }
+    var drawing = CGMutablePath()
+    var lastPhase:UITouch.Phase = .began
+    var lastPoint = CGPoint.zero
     
+    func follow(touch:UITouch) {
+        let loc = touch.location(in: self)
+        switch touch.phase {
+        case .began:
+            drawing.move(to: loc)
+            timer?.invalidate()
+        case .moved:
+            drawing.addLine(to: loc)
+        case .ended:
+            if lastPhase == .began {
+                drawing = CGMutablePath()
+                del?.onTap(view: self)
+            } else {
+                timer?.invalidate()
+                timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: {[unowned self] (tmr) in
+                    let res = recognize(paths: seperate(path: self.drawing))
+                    let most = mostLikely(sign: res.0, results: res.1)
+                    print(most)
+                    if let i = Int(most) {
+                        self.del?.changeto(uid: self.exp.uid, to: NumExp(i))
+                    } else {
+                        self.drawing = CGMutablePath()
+                        self.setNeedsDisplay()
+                        self.latex.isHidden = true
+                    }
+                })
+            }
+        case .cancelled:
+            drawing = CGMutablePath()
+        default:
+            drawing = CGMutablePath()
+        }
+        lastPhase = touch.phase
+        lastPoint = loc
+        latex.isHidden = !drawing.isEmpty
+        
+        setNeedsDisplay()
+    }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         guard let touch = touches.first else {return}
-        overwriter.follow(touch: touch, anchorView: self)
-        setNeedsDisplay()
-        timer?.invalidate()
+        follow(touch: touch)
     }
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesMoved(touches, with: event)
         guard let touch = touches.first else {return}
-        overwriter.follow(touch: touch, anchorView: self)
-        setNeedsDisplay()
+        follow(touch: touch)
     }
     var timer:Timer? = nil
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesEnded(touches, with: event)
         guard let touch = touches.first else {return}
-        if overwriter.lastPhase == .began {
-            self.overwriter.reset()
-            self.setNeedsDisplay()
-            del?.onTap(view: self)
-        } else {
-            overwriter.follow(touch: touch, anchorView: self)
-            self.setNeedsDisplay()
-            
-            timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: {[unowned self] (tmr) in
-                let res = recognize(paths: seperate(path: self.overwriter.drawing))
-                let most = mostLikely(sign: res.0, results: res.1)
-                
-                if let i = Int(most) {
-                    self.del?.changeto(uid: self.exp.uid, to: NumExp(i))
-                    return
-                }
-                
-                self.overwriter.reset()
-                self.setNeedsDisplay()
-            })
-        }
+        follow(touch: touch)
     }
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesCancelled(touches, with: event)
-        overwriter.reset()
-        setNeedsDisplay()
+        guard let touch = touches.first else {return}
+        follow(touch: touch)
     }
     
     var exp:Exp = Unassigned("z")
